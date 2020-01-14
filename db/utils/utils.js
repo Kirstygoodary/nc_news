33dcc141633ac8e50009f39c32f930d12c7acfed
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-01-14 11:29:52 +0000

    created utils file with test

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,39 @@
-exports.formatDates = list => {};
+exports.formatDates = datum => {
+  if(!datum.length) {
+    return []
+  }
+  let copiedData = JSON.parse(JSON.stringify(datum));
+  //let test = new Date(datum[0].created_at).toString();
 
-exports.makeRefObj = list => {};
+  copiedData.forEach(function(data) {
+    let formattedDate = new Date(data.created_at).toJSON();
+    //console.log(formattedDate, "FORMATTED DATE");
 
-exports.formatComments = (comments, articleRef) => {};
+    data.created_at = formattedDate;
+    //console.log(copiedData, "<<<<<COPIED DATA");
+  });
+  return copiedData;
+};
+
+exports.makeRefObj = datum => {
+  if(!datum.length) {
+    return {}
+  }
+  const obj = {};
+  datum.forEach(data => {
+    console.log(data.topic);
+    console.log(data.author_id);
+    obj[data.topic] = data.author_id;
+  });
+  return obj;
+};
+
+exports.formatComments = (commentsData, articleRef) => {
+
+  const formattedCommentsData = [];
+
+  commentsData.forEach(comment => {
+    const commentsCopy = { ...comment };
+    commentsCopy.comment_id = articleRef[commentsCopy.]
+  });
+};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,12 +1,210 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 const {
   formatDates,
   makeRefObj,
-  formatComments,
-} = require('../db/utils/utils');
+  formatComments
+} = require("../db/utils/utils");
 
-describe('formatDates', () => {});
+describe("formatDates", () => {
+  it("returns an empty array when passed an empty object", () => {
+    const data = [];
+    const actual = formatDates(data);
+    expect(actual).to.eql([]);
+  });
 
-describe('makeRefObj', () => {});
+  it("reformats the timestamp value to a stringified date, for one object in the array", () => {
+    const data = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    const actual = formatDates(data);
+    const expected = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: "2018-11-15T12:21:54.171Z",
+        votes: 100
+      }
+    ];
+    expect(actual).to.eql(expected);
+  });
 
-describe('formatComments', () => {});
+  it("reformats the date to a stringified date, for multiple arrays", () => {
+    const data = [
+      {
+        title: "Eight pug gifs that remind me of mitch",
+        topic: "mitch",
+        author: "icellusedkars",
+        body: "some gifs",
+        created_at: 1289996514171
+      },
+      {
+        title: "Student SUES Mitch!",
+        topic: "mitch",
+        author: "rogersop",
+        body:
+          "We all love Mitch and his wonderful, unique typing style. However, the volume of his typing has ALLEGEDLY burst another students eardrums, and they are now suing for damages",
+        created_at: 1163852514171
+      },
+      {
+        title: "UNCOVERED: catspiracy to bring down democracy",
+        topic: "cats",
+        author: "rogersop",
+        body: "Bastet walks amongst us, and the cats are taking arms!",
+        created_at: 1037708514171
+      }
+    ];
+    const actual = formatDates(data);
+    const expected = [
+      {
+        title: "Eight pug gifs that remind me of mitch",
+        topic: "mitch",
+        author: "icellusedkars",
+        body: "some gifs",
+        created_at: "2010-11-17T12:21:54.171Z"
+      },
+      {
+        title: "Student SUES Mitch!",
+        topic: "mitch",
+        author: "rogersop",
+        body:
+          "We all love Mitch and his wonderful, unique typing style. However, the volume of his typing has ALLEGEDLY burst another students eardrums, and they are now suing for damages",
+        created_at: "2006-11-18T12:21:54.171Z"
+      },
+      {
+        title: "UNCOVERED: catspiracy to bring down democracy",
+        topic: "cats",
+        author: "rogersop",
+        body: "Bastet walks amongst us, and the cats are taking arms!",
+        created_at: "2002-11-19T12:21:54.171Z"
+      }
+    ];
+    expect(actual).to.eql(expected);
+  });
+  it("does not mutate the original data", () => {
+    const data = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    expect(formatDates(data)).to.not.equal(data);
+    expect(data).to.eql([
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ]);
+  });
+});
+
+// test for mutated data!!!!!
+
+describe("makeRefObj", () => {
+  it("returns an empty object when passed an empty array", () => {
+    const data = [];
+    expect(makeRefObj(data).to.eql({}));
+  });
+  it("returns the reference object with the key value pairs for one topic", () => {
+    const data = [
+      {
+        author_id: 1,
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    const actual = makeRefObj(data);
+    const expected = { mitch: 1 };
+    expect(actual).to.eql(expected);
+  });
+  it("returns a reference object when passed multiple objects in the array", () => {
+    const data = [
+      {
+        author_id: 1,
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      },
+      {
+        author_id: 2,
+        title: "UNCOVERED: catspiracy to bring down democracy",
+        topic: "cats",
+        author: "rogersop",
+        body: "Bastet walks amongst us, and the cats are taking arms!",
+        created_at: 1037708514171
+      }
+    ];
+    const actual = makeRefObj(data);
+    const expected = { mitch: 1, cats: 2 };
+    expect(actual).to.eql(expected);
+  });
+  it("does not mutate the original data", () => {
+    const data = [
+      {
+        author_id: 1,
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    expect(data).to.eql([
+      {
+        author_id: 1,
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ]);
+  });
+});
+
+describe("formatComments", () => {
+  it("returns and empty array when given an empty array of comments", () => {
+    expect(formatComments([]).to.eql([]));
+  });
+  it("when given one object in the array, it will format 'created_by' to use 'author_id' instead", () => {
+    const commentsData = [
+      {
+        comment_id: 1,
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: 1511354163389
+      }
+    ];
+    const articleRef = { Mitch: 1 };
+    const expected = [{ author_id: 1 }];
+    expect(formatComments(commentsData, articleRef).to.eql(expected));
+  });
+});
