e13f24475b829cfcd5397ae4b521aad8fb9acdf3
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-01-14 11:21:56 +0000

    completed exporting my data

---
+++ db/data/index.js
@@ -0,0 +1,4 @@
+const devData = require("../data/development-data/index");
+const testData = require("../data/test-data/index");
+
+exports.process.env.NODE_ENV = "test";
---
+++ db/data/test-data/index.js
@@ -0,0 +1,4 @@
+exports.topicData = require("../data/test-data/topics");
+exports.articleData = require("../data/test-data/articles");
+exports.userData = require("../data/test-data/users");
+exports.commentData = require("../data/test-data/comments");
