7de4c1803f682b7b5fadcf61557a5133aafddadf
Kirsty Goodary <kirstygoodary1@gmail.com>
2020-02-15 23:29:38 +0000

    changed selectArticles function

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ app.use(function(err, req, res, next) {
     }
   }
   {
-    return res.status(400).send({ msg: "Bad request" });
+    return res.status(400).send({
+      message: "Bad request",
+      error: err.message
+    });
   }
 }); // generic error handler
 
